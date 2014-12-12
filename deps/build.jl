5564d5052f3bd509a720a9b3c84b16a4b9f1c1c7
Tim Lovorn <tflovorn@crimson.ua.edu>
2014-12-12 00:07:37 -0600

    update build script to use submodule

--- deps/build.jl
+++ deps/build.jl
@@ -1,22 +1,13 @@
-# Build script based off: https://github.com/stevengj/Cubature.jl/blob/master/deps/build.jl
 using BinDeps
 
-# version of Cuba package to use
-cubavers = "4.1"
-
-tagfile = "installed_vers"
-if !isfile(tagfile) || readchomp(tagfile) != cubavers
-    if OS_NAME == :Linux
-        if !isfile("Cuba-$cubavers.tar.gz")
-            run(download_cmd("http://www.feynarts.de/cuba/Cuba-$cubavers.tar.gz", "Cuba-$cubavers.tar.gz"))
-        end
-        run(unpack_cmd("Cuba-$cubavers.tar.gz", ".", ".gz", ".tar"))
-        cd("Cuba-$cubavers") do
-            run(`./configure`)
-            run(`make`)
-        end
-    else
-        println("Non-Linux OS not currently supported.")
+if OS_NAME == :Linux
+    run(`git submodule init`)
+    run(`git submodule update`)
+    cd("cuba") do
+        run(`./configure`)
+        run(`cp makefile.shared makefile`)
+        run(`make`)
     end
-    run(`echo $cubavers` |> tagfile)
+else
+    println("Non-Linux OS not currently supported.")
 end
